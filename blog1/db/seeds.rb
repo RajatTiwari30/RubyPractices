4b0bcf33eebea190d8eadc288530dd8c88be56c8
RajatTiwari30 <tiwari.rajat1098@gmail.com>
2020-09-14 13:13:02 +0530

    Seed Updated

--- blog1/db/seeds.rb
+++ blog1/db/seeds.rb
@@ -6,11 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Tag.delete_all
+Post.delete_all
 Category.delete_all
 
 Tag.create(name: "javascript")
 Tag.create(name: "react")
 Tag.create(name: "ruby")
 Tag.create(name: "rails")
-Category.create(name: "web development", description: "All about Web development")
-Category.create(name: "database", description: "All about Database")
+cat_wd=Category.create(name:"web development", description:"All about Web development", published:true)
+cat_db=Category.create(name:"database", description:"All about Database", published:true)
+
+10.times do |i|
+    Post.create(
+        Title: "Post #{i}",
+        Body: "Body #{i}",
+        Published:true,
+        category_id: i%2==0? cat_wd.id : cat_db.id
+    )
+end
