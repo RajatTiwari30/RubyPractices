c63b75a5c7abb70512a93f66fd0fbab5799e329b
RajatTiwari30 <tiwari.rajat1098@gmail.com>
2020-09-15 16:00:16 +0530

    Devise sign in sign out added

--- blog1/app/models/post.rb
+++ blog1/app/models/post.rb
@@ -6,4 +6,21 @@ class Post < ApplicationRecord
     scope :published, -> { where(published:true) }
     scope :unpublished, -> { where.not(published:true) }
     scope :order_by_latest, -> { reorder(created_at: :desc) }
+
+    def all_tags=(tag_names)
+        if tag_names.blank?
+            return
+        end
+        self.tags=tag_names.split(",").map do |tag_name|
+            unless tag_name.blank?
+                Tag.where(name: tag_name.strip).first_or_create!
+            end
+        end
+    end
+    
+    def all_tags
+        tags.map(&:name).join(",")
+    end
+
+
 end
