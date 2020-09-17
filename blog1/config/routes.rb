09e6ce232852f2db2d3234b8cb7e3fb981609cff
RajatTiwari30 <tiwari.rajat1098@gmail.com>
2020-09-17 23:02:42 +0530

    API added

--- blog1/config/routes.rb
+++ blog1/config/routes.rb
@@ -1,20 +1,34 @@
 Rails.application.routes.draw do
   devise_for :users
   post "home/comments/:post_id", to: "home#comments", as: :comments
-  resources :posts
+
+  scope :admin do
+    resources :posts
+    resources :categories
+    resources :tags
+  end
+
   get 'tags/index'
-  resources :categories
-  resources :tags
-  get "home/search", to: "home#search", :as => :post_search
-  get 'home/read/:id', to: 'home#read', :as => :post_read
-  get 'home/:tag/posts', to: 'home#tag_search', :as => :tag_search
   get 'category/index'
   get 'category/show'
-  get 'home/posts/:category', to: 'home#category_search_post', :as => :category_search_post
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-root 'home#index'
-get 'home/ok'
-get 'home/index'
-get 'home/about', as: :about
-get 'home/contact', as: :contact
+  namespace :api do
+    get "posts", to: "posts#index"
+    get "posts/:id", to: "posts#show"
+    post "posts", to: "posts#create"   
+    delete "posts/:id", to:"posts#destroy" 
+  end
+  
+  scope :public do 
+    get "home/search", to: "home#search", :as => :post_search
+    get "home/read/:id", to: "home#read", :as => :post_read
+    get "home/:tag/posts", to: "home#tag_search", :as => :tag_search
+    get 'home/posts/:category', to: 'home#category_search_post', :as => :category_search_post
+    get 'home/ok'
+    get 'home/index'
+    get 'home/about', as: :about   #about_path or about_url
+    get 'home/contact', as: :contact
+  end
+
+  root 'home#index'
 end
